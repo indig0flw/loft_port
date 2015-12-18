b63bf44d4d9bc218ebb56c808551bd205d38e842
Anton Lalakin <anton.lalakin@yandex.ru>
2015-12-18 13:31:18 +0300

    v1.2.0 Modal and login windows added, JS added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,35 @@
-$(document).ready(function() {
-	console.log('мы на главной');
-});
+var myModule = (function() {
+
+    var init = function() {
+        _setUpListeners();
+    };
+
+    var _setUpListeners = function() {
+        $('.add-new').on('click', _showModal);
+        $('.login-link').on('click', _showLogin); // открыть по клику
+    };
+
+    var _showModal = function(prev) {
+        console.log('клик работает');
+        prev.preventDefault();
+        $('.modal-wrapper').bPopup({
+            speed: 650,
+            transition: 'slideIn',
+            transitionClose: 'slideBack'
+        });
+    };
+
+    var _showLogin = function (prev) {
+    	console.log('login работает')
+    	prev.preventDefault();
+    	$('.login-wrapper').bPopup();
+    };
+
+
+    return {
+        init: init
+    };
+
+})();
+
+myModule.init();
